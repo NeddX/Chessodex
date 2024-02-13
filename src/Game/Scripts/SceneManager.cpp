915fe651d3f22a2374c013769bfe066c886b7f78
NeddX <nedbeargt@gmail.com>
2024-02-13 16:35:47 +0400

    Minor

--- src/Game/Scripts/SceneManager.cpp
+++ src/Game/Scripts/SceneManager.cpp
@@ -6,27 +6,35 @@
 namespace chx {
     CODEX_USE_ALL_NAMESPACES()
 
+    namespace fs = std::filesystem;
+
+    auto LoadSprite(const fs::path& path)
+    {
+        return Resources::Load<Texture2D>(Chessodex::GetAppDataPath() / "Sprites/" / path);
+    }
+
     void SceneManager::Init()
     {
+        // Load our resources.
         // As of now you cannot render solid colours with Codex
         // so your best bet is to load a solid white image and
         // do whatever is it that you want to do.
+        m_Resources["solid_white"] = LoadSprite("solid_white.png");
+        m_Resources["pieces"] = LoadSprite("pieces.png");
 
-        // Load our resources.
-        m_Resources["solid_white"] =
-            Resources::Load<Texture2D>(Chessodex::GetAppDataPath() / "Sprites/solid_white.png");
-
-        DrawChessboard();
+        CreateBoard();
+        CreatePieces("soemthing here");
     }
 
     void SceneManager::Update(const codex::f32 deltaTime)
     {
     }
 
-    void SceneManager::DrawChessboard()
+    void SceneManager::CreateBoard()
     {
         static unsigned board_size = 8;
         static unsigned tile_size  = 60;
+        static unsigned offset     = 30;
 
         for (auto y = 0; y < board_size; ++y)
         {
@@ -35,15 +43,21 @@ namespace chx {
                 m_Chessboard.push_back(CreateEntity(fmt::format("tile_{}_{}", x, y)));
 
                 auto& c    = m_Chessboard.back().GetComponent<TransformComponent>();
-                c.position = { x + tile_size, y + tile_size, 0 };
+                c.position = { offset + x * tile_size, offset + y * tile_size, 0 };
 
                 bool is_white = (x + y) % 2 == 0;
 
-                static auto sprite      = Sprite(m_Resources["solid_white"]);
+                auto sprite      = Sprite(m_Resources["solid_white"]);
                 auto&       sprite_comp = m_Chessboard.back().AddComponent<SpriteRendererComponent>(sprite);
+                sprite_comp.GetSprite().SetSize({ tile_size, tile_size });
                 sprite_comp.GetSprite().GetColour() =
                     (is_white) ? Vector4f{ 1.0f, 1.0f, 1.0f, 1.0f } : Vector4f{ 0.0f, 0.0f, 0.0f, 1.0f };
             }
         }
+    }
+
+    void SceneManager::CreatePieces(const std::string_view fmt)
+    {
+
     }
 } // namespace chx
--- src/Game/Scripts/SceneManager.h
+++ src/Game/Scripts/SceneManager.h
@@ -6,12 +6,15 @@ namespace chx {
     class SceneManager : public codex::NativeBehaviour
     {
     private:
-        std::vector<codex::Entity>                                                 m_Chessboard{};
         std::unordered_map<std::string, codex::ResRef<codex::graphics::Texture2D>> m_Resources{};
+        std::vector<codex::Entity>                                                 m_Chessboard{};
+        std::vector<codex::Entity>                                                 m_WhitePieces{};
+        std::vector<codex::Entity>                                                 m_BlackPieces{};
 
     protected:
         void Init() override;
         void Update(const codex::f32 deltaTime) override;
-        void DrawChessboard();
+        void CreateBoard();
+        void CreatePieces(const std::string_view fmt);
     };
 } // namespace chx
